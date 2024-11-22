b58ddb4169be3090484abafbf2196080593431d2
troubleman96 <itslugenge96@gmail.com>
2024-11-23 00:03:05 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Luge
+{
+    class Car{
+        public string model;
+
+        public Car(string modelName){
+            model = modelName;
+        }
+        static void Main(string [] arg){
+            Car ford = new Car("Mustang");
+            Console.WriteLine(ford.model);
+        }
+
+    }
+}
